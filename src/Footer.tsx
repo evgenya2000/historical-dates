7a9b70ffd3d274fe243fd1fd44ae1070a700468c
evgenya2000 <evgenya.2000@yandex.ru>
2023-12-15 21:35:02 +0300

    finish animation

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,6 +1,8 @@
 import './Footer.scss';
-import { useState } from 'react';
-import { years } from './_data';
+import { useEffect, useState } from 'react';
+import { years, data } from './_data';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
 
 function Footer() {
   const numPoint: number = 6;
@@ -9,26 +11,39 @@ function Footer() {
   const diameterCircle: number = 450;
   /*   const diameterPoint: number = 10; */
   const [currentPoint, setCurrentPoint] = useState(1);
+  let swiper: Swiper | null = null;
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    swiper = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 100,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }, []);
 
   async function changeOfYears(numSelected: number) {
     function sleep(ms: number) {
       return new Promise(resolve => setTimeout(resolve, ms));
     }
-    
+
     let counter = currentPoint - 1;
     const time = 100;
     if (counter > numSelected) {
       do {
         counter--;
         document.getElementsByClassName("first-year")[0].innerHTML = years[counter].first;
-        document.getElementsByClassName("second-year")[0].innerHTML = years[counter].second;
+        document.getElementsByClassName("second-year")[0].innerHTML = years[counter].last;
         await sleep(time);
       } while (numSelected !== counter);
     } else {
       do {
         counter++;
         document.getElementsByClassName("first-year")[0].innerHTML = years[counter].first;
-        document.getElementsByClassName("second-year")[0].innerHTML = years[counter].second;
+        document.getElementsByClassName("second-year")[0].innerHTML = years[counter].last;
         await sleep(time);
       } while (numSelected !== counter);
     }
@@ -71,6 +86,26 @@ function Footer() {
     }
   }
 
+  function renderSlider() {
+    const currentYears = years[currentPoint - 1].first + years[currentPoint - 1].last;
+
+    return (
+      <div className='swiper-container'>
+        <div className='swiper-wrapper'>
+          {data[currentYears].map((blockInfo) => (
+            <div className='swiper-slide' key={blockInfo.year}>
+              <p className='year'>{blockInfo.year}</p>
+              <p className='info'>{blockInfo.info}</p>
+            </div>
+          ))}
+        </div>
+        {/* Кнопки навигации добавлены как дочерние элементы к swiper-container */}
+        <div className='swiper-button-prev' onClick={() => swiper?.slidePrev()}></div>
+        <div className='swiper-button-next' onClick={() => swiper?.slideNext()}></div>
+      </div>
+    );
+  }
+
   return (
     <div className="footer">
       <div className='container-circle'>
@@ -83,7 +118,7 @@ function Footer() {
         </div>
         <div className='container-years'>
           <p className='first-year'>{years[0].first}</p>
-          <p className='second-year'>{years[0].second}</p>
+          <p className='second-year'>{years[0].last}</p>
         </div>
         <p className='current-point'>{`0${currentPoint}/0${numPoint}`}</p>
         <div className='container-button'>
@@ -91,6 +126,7 @@ function Footer() {
           <div className={`arrow arrow-right ${currentPoint === numPoint ? "btn-off" : "btn-on"}`} onClick={() => rotateRight()}></div>
         </div>
       </div>
+      {renderSlider()}
     </div>
   );
 }
--- src/_data.js
+++
@@ -1,2 +0,0 @@
-export const years = [{"first": "2000", "second": "2004"}, {"first": "2005", "second": "2009"}, {"first": "2010", "second": "2014"}, {"first": "2015", "second": "2019"}, {"first": "2020", "second": "2024"}, {"first": "2025", "second": "2029"}];
-
