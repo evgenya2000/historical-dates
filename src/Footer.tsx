e00bb3f4416f4d9e33fb89135b42085eb5e9554f
evgenya2000 <evgenya.2000@yandex.ru>
2023-12-15 14:07:50 +0300

    circle finish

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,52 +5,38 @@ function Footer() {
   const arcAngle: number = 360 / numPoint;
   const shift: number = 60;
   const diameterCircle: number = 450;
+  /*   const diameterPoint: number = 10; */
 
   function rotateToSelectedPoint(selectedPoint: any) {
     let rotationValue = (-parseInt(selectedPoint) + 1) * arcAngle;
     const points = document.querySelectorAll('.point') as NodeListOf<HTMLElement>;
-    
+
     points.forEach((point, index) => {
-      if (point.id === "current"){
+      if (point.id === "current") {
         point.id = `point${index + 1}`
-      } 
+      }
       let pointRotation = index * arcAngle + rotationValue - shift;
       const currentTransform = `translate(-50%, -50%) rotate(${pointRotation}deg) translate(${diameterCircle / 2}px) rotate(${-pointRotation}deg)`;
-
-      let mouseEnterHandler = function () {
-        if (point.id !== "current") {
-          point.style.transform = point.style.transform.split(" ").slice(0, 5).join(" ") + "scale(6)"; 
-        }
-      }
-      let mouseLeaveHandler = function() {
-        if (point.id !== "current") {
-          point.style.transform = point.style.transform.split(" ").slice(0, 5).join(" ") + "scale(1)";
-          point.style.backgroundColor = '$black-blue';
-        }
-      }
-      if (index + 1 === selectedPoint) {
-        point.style.transform = currentTransform;
-        point.id = "current";
-        point.removeEventListener('mouseenter', mouseEnterHandler);
-        point.removeEventListener('mouseleave', mouseLeaveHandler);
-      } else {
-        point.style.transform = currentTransform;
-        point.addEventListener("mouseenter", mouseEnterHandler);
-        point.addEventListener("mouseleave", mouseLeaveHandler);
-      }
       point.style.transform = currentTransform;
+      if (index + 1 === selectedPoint) {
+        point.id = "current";
+      }
     });
-  }  
- 
+  }
+
   return (
     <div className="footer">
       <div className='container-circle'>
         <div className="circle">
-        {Array.from({length: numPoint}, (_, index) => index + 1).map((point, index) => (
-          <div className="point" id={`${index === 0 ? 'current' : `point${index + 1}`}`} key={point} onClick={() => rotateToSelectedPoint(point)}>
-            <p>{point}</p>
-          </div>
-        ))}
+          {Array.from({ length: numPoint }, (_, index) => index + 1).map((point, index) => (
+            <div className="point" id={`${index === 0 ? 'current' : `point${index + 1}`}`} key={point} onClick={() => rotateToSelectedPoint(point)}>
+              <p>{point}</p>
+            </div>
+          ))}
+        </div>
+        <div className='container-button'>
+          <div className='arrow arrow-left'></div>
+          <div className='arrow arrow-right'></div>
         </div>
       </div>
     </div>
